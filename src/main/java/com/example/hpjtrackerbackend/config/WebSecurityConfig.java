f565de7e06cc5def85acb851e0c7d62646ce6069
Haru Lee <haru.lee@gm.com>
2021-02-21 16:33:12 -0600

    if basic auth works (just one pair of credentials from application.properties file), then one can access all endpoints

--- src/main/java/com/example/hpjtrackerbackend/config/WebSecurityConfig.java
+++ src/main/java/com/example/hpjtrackerbackend/config/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.hpjtrackerbackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,15 +13,31 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
+    @Value("${security.user}")
+    private String user;
+
+    @Value("${security.password}")
+    private String password;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser(user)
+            .password("{noop}" + password);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.cors().and().csrf().disable()
-        .requiresChannel()
-        .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-        .requiresSecure();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic()
+            .and()
+            .cors().and().csrf().disable()
+            .requiresChannel()
+            .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+            .requiresSecure();
     }
 
     @Bean
@@ -32,5 +51,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
