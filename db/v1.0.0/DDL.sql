a12b8d3c830d5c63db6c6bbcd96a2569df6955e4
Haru Lee <haru.lee@gm.com>
2021-02-22 10:12:06 -0600

    completion_units_per_category is ordered by done_date and category_id

--- db/v1.0.0/DDL.sql
+++ db/v1.0.0/DDL.sql
@@ -76,11 +76,12 @@ ALTER TABLE public.entry_with_category_name_and_goal_name OWNER TO hwmvazhkynagf
 CREATE OR REPLACE VIEW public.completion_units_per_category AS
     SELECT calculated.done_date, calculated.category_name, SUM(calculated.percent) AS percent_sum, calculated.goal_in_comparable_unit, SUM(calculated.percent) / calculated.goal_in_comparable_unit * 100 AS completion_percent
     FROM (
-        SELECT *, CAST(LEAST(count, max_count) AS DOUBLE PRECISION) / e.goal_count * e.multiplier AS percent
+             SELECT *, CAST(LEAST(count, max_count) AS DOUBLE PRECISION) / e.goal_count * e.multiplier AS percent, category.id AS category_id
         FROM public.entry_with_category_name_and_goal_name e
 		LEFT JOIN category
 		ON category.name = e.category_name
     ) calculated
-    GROUP BY calculated.done_date, calculated.category_name, calculated.goal_in_comparable_unit;
+    GROUP BY calculated.done_date, calculated.category_id, calculated.category_name, calculated.goal_in_comparable_unit
+    ORDER BY calculated.done_date desc, calculated.category_id;
 
 ALTER TABLE public.completion_units_per_category OWNER TO hwmvazhkynagfn;
