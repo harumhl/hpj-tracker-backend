4e770dea679df89ca4fe206c3521c29fb21f9264
Haru Lee <haru.lee@gm.com>
2021-02-22 18:08:14 -0600

    setting timezone as Texas so */today works for my case

--- src/main/java/com/example/hpjtrackerbackend/HpjTrackerBackendApplication.java
+++ src/main/java/com/example/hpjtrackerbackend/HpjTrackerBackendApplication.java
@@ -3,10 +3,13 @@ package com.example.hpjtrackerbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class HpjTrackerBackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT-5")); // Texas
 		SpringApplication.run(HpjTrackerBackendApplication.class, args);
 	}
 
