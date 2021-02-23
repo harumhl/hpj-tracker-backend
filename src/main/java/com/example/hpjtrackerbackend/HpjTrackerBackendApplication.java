205034767dc5b2ab9bf3480aab8e76ab5ad662eb
Haru Lee <haru.lee@gm.com>
2021-02-23 09:00:55 -0600

    apparently timezone set didn't have any effect on heroku server. trying again

--- src/main/java/com/example/hpjtrackerbackend/HpjTrackerBackendApplication.java
+++ src/main/java/com/example/hpjtrackerbackend/HpjTrackerBackendApplication.java
@@ -3,13 +3,17 @@ package com.example.hpjtrackerbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
 public class HpjTrackerBackendApplication {
 
-	public static void main(String[] args) {
+	@PostConstruct
+	public void init(){
 		TimeZone.setDefault(TimeZone.getTimeZone("GMT-5")); // Texas
+	}
+	public static void main(String[] args) {
 		SpringApplication.run(HpjTrackerBackendApplication.class, args);
 	}
 
